3eb6e23c287b2dd007211cde068cb6192d483c8e
Sydney Idundun <76040196+SydneyTechnologies@users.noreply.github.com>
2024-05-21 13:05:56 +0400

    some change made

--- models.py
+++ models.py
@@ -7,50 +7,19 @@ class Report(BaseModel):
 
 
 class WsbData(BaseModel):
-    websiteTitle: str = "Mandy's Website"
+    websiteTitle: str
+    gmbKey: str
+    concept: str
     country: str = ""
     city: str = ""
-    gmbKey: str 
-    group: bool = True
-    concept: str 
-    aboutKeywords: str
-
-    # Optional fields
-    optionSet: list = []
-    submitCount: int = 1
-    defaultColor: dict = {
-        "accentColor": [
-            "HSL",
-            0.01486199575371549,
-            0.6460905349794239,
-            0.5235294117647059,
-            1
-        ],
-        "mainColor": [
-            "HSL",
-            0,
-            0,
-            0.9725490196078431,
-            1
-        ],
-        "whiteColor": [
-            "HSL",
-            0,
-            0,
-            1,
-            1
-        ],
-        "blackColor": [
-            "HSL",
-            0.5645,
-            0,
-            0.23529411764705882,
-            1
-        ]
-    }
-    addressData: dict = {}
+    aboutKeywords: str = ""
     language: str = "en_gb"
-    additionalData: dict 
+    defaultColor: str = "rustic"
+    pronouns: str = "We"
+    optionSet: list[str] = []
+    additionalData: dict = {}
+
+    # constants
+    submitCount: int = 1
     addShopSection: bool = False
 
- 
